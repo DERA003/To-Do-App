a9fb669b5ca9f119144e3078e3aba319c8a65208
Chidera Mabel Umejuru <deraumejuru@gmail.com>
2024-09-23 15:36:10 +0100

    add

--- Dera.js
+++ Dera.js
@@ -3,9 +3,13 @@ const listContainer = document.getElementById("list-container")
 
 
 function addTask() {
+   if(inputBox.value === ''){
+      alert("please write task")
+   }else{
     const li = document.createElement("li")
     li.innerHTML = inputBox.value
     listContainer.appendChild(li)
+   }
 }
 
 addTask()
