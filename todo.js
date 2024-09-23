4acb7b149183d9d6c22ce6a5dad6164f7db88879
Chidera Mabel Umejuru <deraumejuru@gmail.com>
2024-09-23 13:33:47 +0100

    add

--- todo.js
+++ todo.js
@@ -19,5 +19,8 @@ listContainer.addEventListener("click", function(e){
     if(e.target.tagName = "LI"){
         e.target.classList.toggle("checked")
     }
-})
+    else if(e.target.tagName = "SPAN"){
+        e.target.parentElement.remove()
+    }
+},false)
 addTask()
