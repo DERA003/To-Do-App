678b2d3fede177048aa55df3462ed130957c777c
Chidera Mabel Umejuru <deraumejuru@gmail.com>
2024-09-23 15:50:44 +0100

    add

--- Dera.js
+++ Dera.js
@@ -12,6 +12,7 @@ function addTask() {
     const span = document.createElement("span")
     span.innerHTML = "X"
     li.appendChild(span)
+    saveData()
    }
    inputBox.value = ""
 }
@@ -21,7 +22,18 @@ addTask()
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked")
+        saveData()
     }else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove()
+        saveData()
     }
 },false)
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function displayTask() {
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+displayTask()
