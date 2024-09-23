7ab6acc4abbd40508ea0795ee78e6daaedc26569
Chidera Mabel Umejuru <deraumejuru@gmail.com>
2024-09-23 13:42:02 +0100

    add

--- todo.js
+++ todo.js
@@ -11,16 +11,22 @@ function addTask() {
         const span = document.createElement("span")
         span.innerHTML = "X"
         li.appendChild(span)
+        saveData()
     }
     inputBox.value = ""
 }
 
 listContainer.addEventListener("click", function(e){
-    if(e.target.tagName = "LI"){
+    if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked")
+        
     }
-    else if(e.target.tagName = "SPAN"){
+    else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove()
     }
 },false)
 addTask()
+
+function saveData() {
+    localStorage.setItem("data",listContainer.innerHTML)
+}
